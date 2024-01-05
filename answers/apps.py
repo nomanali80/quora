27d6dc610e168efc4570bd7766cf5acfb4f7012c
Noman Ali <nouman.ali@devsinc.com>
2024-01-05 14:19:28 +0500

    email to question user when answered to question

--- answers/apps.py
+++ answers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AnswersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "answers"
+
+    def ready(self):
+        import answers.signals
